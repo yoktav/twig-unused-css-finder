5f2deed000e9cda98692055d117c2911e99b84ce
YilmazOktav <hello@yilmazoktav.com>
2024-09-16 15:07:42 +0300

    Update utils jsdoc

--- src/utils.ts
+++ src/utils.ts
@@ -3,8 +3,10 @@ import * as path from 'path';
 
 /**
  * Logs a message if debugging is enabled
- * @param message - The message to log
- * @param isDebugEnabled - Whether debugging is enabled
+ *
+ * @param {string} message - The message to log
+ * @param {boolean} isDebugEnabled - Whether debugging is enabled
+ * @returns {void}
  */
 export function log(message: string, isDebugEnabled: boolean): void {
   if (!isDebugEnabled) return;
@@ -13,12 +15,14 @@ export function log(message: string, isDebugEnabled: boolean): void {
 
 /**
  * Checks if a class name is valid
- * @param className - The class name to check
- * @returns True if the class name is valid, false otherwise
+ *
+ * Class names must start with a letter, underscore, or hyphen
+ * and can be followed by letters, numbers, underscores, or hyphens
+ *
+ * @param {string} className - The class name to check
+ * @returns {boolean} True if the class name is valid, false otherwise
  */
 export function isValidClassName(className: string): boolean {
-  // Class names must start with a letter, underscore, or hyphen
-  // and can be followed by letters, numbers, underscores, or hyphens
   const CLASS_NAME_REGEX = /^-?[_a-zA-Z]+[_a-zA-Z0-9-]*$/;
   return CLASS_NAME_REGEX.test(className);
 }
@@ -30,9 +34,10 @@ interface FileInfo {
 
 /**
  * Recursively finds files in a directory that match a given pattern
- * @param dir - The directory to search in
- * @param pattern - The pattern to match file names against
- * @returns An array of objects containing file names and paths
+ *
+ * @param {string} dir - The directory to search in
+ * @param {RegExp} pattern - The regular expression pattern to match file names against
+ * @returns {FileInfo[]} An array of FileInfo objects containing file names and paths
  */
 export function findFiles(dir: string, pattern: RegExp): FileInfo[] {
   const files: FileInfo[] = [];
