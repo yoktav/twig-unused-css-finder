4035b13471407030fc82fb452cdf1c93aac3c5f4
YilmazOktav <hello@yilmazoktav.com>
2024-09-17 18:16:23 +0300

    Update build file paths

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/index.js',
     format: 'cjs',
     exports: 'named',
     sourcemap: true,
