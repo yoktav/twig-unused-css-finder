a9007d658d073ca8c20d63d5d309c15635af6f67
YilmazOktav <hello@yilmazoktav.com>
2024-09-17 18:17:49 +0300

    Add terser to rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts',
@@ -13,7 +14,8 @@ export default {
   plugins: [
     typescript(),
     nodeResolve(),
-    commonjs()
+    commonjs(),
+    terser()
   ],
   external: ['fs', 'path']
 };
